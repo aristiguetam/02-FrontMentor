94e96beb0fd334627921119f32ce1e9cd4277dd1
aristiguetam <aristiguetam97@gmail.com>
2023-07-02 12:35:36 -0500

    priority images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata = {
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="/images/favicon.png" />
+      </head>
       <body>
         <Providers>
 
